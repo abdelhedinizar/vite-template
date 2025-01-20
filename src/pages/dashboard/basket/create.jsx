ddce4207384a0e0fc6cdd63e3befe48597712eba
abdelehdinizar <abdelhedinizarcss@gmail.com>
2025-01-20 17:39:33 +0100

    Home page

--- src/pages/dashboard/basket/create.jsx
+++ src/pages/dashboard/basket/create.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Card, CardContent, CardMedia, IconButton, Link, Stack, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, CardMedia, IconButton, Link, Stack, Typography } from '@mui/material';
 import { ArrowLeft as ArrowLeftIcon } from '@phosphor-icons/react/dist/ssr/ArrowLeft';
 import { Heart as HeartIcon } from '@phosphor-icons/react/dist/ssr/Heart';
 import { Helmet } from 'react-helmet-async';
@@ -71,6 +71,93 @@ export function Page() {
             </div>
           </Stack>
         </Stack>
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          maxWidth: 'var(--Content-maxWidth)',
+          bottom: 0,
+          boxShadow: 'rgba(0, 0, 0, 0.1) 0px 10px 50px; !important;',
+          width: 'var(--Content-width)',
+        }}
+      >
+        <Card
+          sx={{
+            borderRadius: 0,
+            overflow: 'hidden',
+          }}
+        >
+          <CardContent>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-between' }}
+            >
+              <Stack direction="row">
+                <Button
+                  sx={{
+                    backgroundColor: 'var(--mui-palette-primary-700)',
+                    color: 'white',
+                    height: '40px',
+                    width: '40px',
+                    borderRadius: '25%',
+                    fontWeight: 'bold',
+                    boxShadow: '0px 4px 6px var(--mui-palette-primary-300)', // Subtle shadow
+                    '&:hover': {
+                      backgroundColor: 'var(--mui-palette-primary-800)', // Slightly darker shade on hover
+                    },
+                  }}
+                >
+                  -
+                </Button>
+                <Typography
+                  color="text.secondary"
+                  sx={{
+                    alignItems: 'center',
+                    margin: 'auto',
+                    p: 1,
+                    color: 'var(--mui-palette-secondary-800)',
+                    fontWeight: 'bold',
+                  }}
+                  variant="h6"
+                >
+                  1x
+                </Typography>
+
+                <Button
+                  sx={{
+                    backgroundColor: 'var(--mui-palette-primary-700)',
+                    color: 'white',
+                    height: '40px',
+                    width: '40px',
+                    fontWeight: 'bold',
+                    borderRadius: '25%',
+                    boxShadow: '0px 4px 6px var(--mui-palette-primary-300)', // Subtle shadow
+                    '&:hover': {
+                      backgroundColor: 'var(--mui-palette-primary-800)', // Slightly darker shade on hover
+                    },
+                  }}
+                >
+                  +
+                </Button>
+              </Stack>
+
+              <Button
+                sx={{
+                  backgroundColor: 'var(--mui-palette-primary-700)',
+                  color: 'white',
+                  mx: 'auto',
+                  boxShadow: '0px 4px 6px var(--mui-palette-primary-300)', // Subtle shadow
+                  '&:hover': {
+                    backgroundColor: 'var(--mui-palette-primary-800)', // Slightly darker shade on hover
+                  },
+                }}
+              >
+                Order for €{selectedDish.price}
+              </Button>
+            </Stack>
+          </CardContent>
+        </Card>
       </Box>
     </React.Fragment>
   );
