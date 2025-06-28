8e9a46c99ea51737afaf01258ca127191fe9cd45
abdelehdinizar <abdelhedinizarcss@gmail.com>
2025-06-28 17:46:39 +0200

    prodectivity marketing + contact + traduire en français

--- src/components/core/logo.jsx
+++ src/components/core/logo.jsx
@@ -13,9 +13,9 @@ export function Logo({ color = 'dark', emblem, height = HEIGHT, width = WIDTH })
   let url;
 
   if (emblem) {
-    url = color === 'light' ? '/assets/logo-emblem.svg' : '/assets/logo-emblem--dark.svg';
+    url = color === 'light' ? '/assets/servy_logo.png' : '/assets/logo-emblem--dark.svg';
   } else {
-    url = color === 'light' ? '/assets/servy_logo.png' : '/assets/logo--dark.svg';
+    url = color === 'light' ? '/assets/servy_logo.png' : '/assets/servy_logo_dark.png';
   }
 
   return <Box alt="logo" component="img" height={height} src={url} width={width} />;
